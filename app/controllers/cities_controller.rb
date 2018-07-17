a37e93e1e776656a22aaaa3f8bdf2b0e9268a96b
timfesta <timothyfesta@yahoo.com>
2018-07-17 05:43:20 -0700

    added san diego

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,5 +3,9 @@ class CitiesController < ApplicationController
 	def sanfranciso
 		
 	end
+
+	def sandiego
+		
+	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
    get 'cities' => 'cities#index'
+
    get 'sanfrancisco' => 'cities#sanfrancisco'
+   get 'sandiego' => 'cities#sandiego'
+   
    get 'states' => 'states#index'
 
   root 'static#index'
